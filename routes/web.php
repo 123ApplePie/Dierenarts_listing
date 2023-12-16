8a282284fe6259def43750eab5098acff9dafe79
123ApplePie <iqgeniuslab@gmail.com>
2023-12-16 10:12:08 -0300

    Jetstream added for auth

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,12 @@ Route::get('/', function () {
 });
 
 Route::view('/test','test');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
